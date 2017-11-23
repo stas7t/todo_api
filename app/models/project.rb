211621bd340c55579ce91a171201ec40ea9f9281
Stas Tischenko <stas7t@gmail.com>
2017-11-23 21:28:47 +0000

    Fix project uniqueness validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: {scope: :user}
 end
