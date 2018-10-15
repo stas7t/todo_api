51bfc1909b0bbc9a018d12acd0598445ff3e3039
stas7t <stas7t@gmail.com>
2018-10-16 01:41:56 +0300

    add v2 api

---
+++ app/controllers/api/v2/projects_controller.rb
@@ -0,0 +1,73 @@
+class Api::V2::ProjectsController < ApplicationController
+  load_and_authorize_resource through: :current_user
+
+  resource_description do
+    short 'Projects'
+    formats ['json']
+  end
+
+  # GET /projects
+  # GET /projects.json
+  api :GET, "/projects", "Get list of projects"
+
+  def index
+    render json: @projects.as_json(include: :tasks)
+  end
+
+  # GET /projects/1
+  # GET /projects/1.json
+  api :GET, "/projects/:id", "Show specific project"
+  param :id, String, "ID of the project", required: true
+
+  def show
+    render json: @project.as_json
+  end
+
+  # POST /projects
+  # POST /projects.json
+  api :POST, "/projects", "Create a project"
+  param :name, String, "Name of the project", required: true
+
+  def create
+    @project = current_user.projects.build(project_params)
+
+    if @project.save
+      render json: @project.as_json, status: :created
+    elsif current_user.projects.find_by(name: params[:name])
+      render json: { message: 'The project with such name does already exist.' }, status: :conflict
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /projects/1
+  # PATCH/PUT /projects/1.json
+  api :PUT, "/projects/:id", "Update a project"
+  param :id, String, "ID of the project", required: true
+  param :name, String, "Name of the project", required: true
+
+  def update
+    if @project.update(project_params)
+      render json: @project.as_json, status: :ok
+    elsif current_user.projects.find_by(name: params[:name])
+      render json: { message: 'The project with such name does already exist.' }, status: :conflict
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /projects/1
+  # DELETE /projects/1.json
+  api :DELETE, "/projects/:id", "Delete a project"
+  param :id, String, "ID of the project", required: true
+
+  def destroy
+    @project.destroy
+  end
+
+  private
+
+  def project_params
+    params.permit(:name, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,23 @@ Rails.application.routes.draw do
       resources :comments, only: %i[show update destroy]
       resources :deadlines, only: %i[show update destroy]
     end
+
+    namespace :v2 do
+      namespace :auth do
+        post 'register', to: 'users#register'
+        post 'login',    to: 'users#login'
+      end
+
+      root to: 'projects#index'
+      resources :projects do
+        resources :tasks, only: %i[index create] do
+          resources :comments, only: %i[index create]
+          resources :deadlines, only: %i[index create]
+        end
+      end
+      resources :tasks, only: %i[show update destroy]
+      resources :comments, only: %i[show update destroy]
+      resources :deadlines, only: %i[show update destroy]
+    end
   end
 end
